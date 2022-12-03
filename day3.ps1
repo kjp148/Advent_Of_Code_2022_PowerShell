09d9c96db24c585fecf8952f3c3b93957da24f5f
Keanan Paul <keananpaul@yahoo.com>
2022-12-03 03:28:27 -0800

    Created hashtable with char values.  Seems I can't display the values using the keys for some reason.

--- day3.ps1
+++ day3.ps1
@@ -5,3 +5,11 @@ Get-Content .\Input\day3.txt | ForEach-Object {
         b = $_.Substring($_.Length / 2)
     }
 }
+
+# Create hashtable of char values
+$CharTable = @{}
+for ($i = 0; $i -lt 26; $i++)
+{
+    $CharTable.$([char](97+$i)) = 1+$i # Lowercase (a = 97)
+    $CharTable.$([char](65+$i)) = 27+$i # Uppercase (A = 65)
+}
