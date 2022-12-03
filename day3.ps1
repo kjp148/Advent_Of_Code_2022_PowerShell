16acbb2a6f24045d2c79286b57f62e699e03a87a
Keanan Paul <keananpaul@yahoo.com>
2022-12-03 03:32:34 -0800

    Fixed CharTable.  Had to cast the key char as a string.

--- day3.ps1
+++ day3.ps1
@@ -10,6 +10,6 @@ Get-Content .\Input\day3.txt | ForEach-Object {
 $CharTable = @{}
 for ($i = 0; $i -lt 26; $i++)
 {
-    $CharTable.$([char](97+$i)) = 1+$i # Lowercase (a = 97)
-    $CharTable.$([char](65+$i)) = 27+$i # Uppercase (A = 65)
+    $CharTable.Add($([string][char](97+$i)), 1+$i) # Lowercase (a = 97)
+    $CharTable.$([string][char](65+$i)) = 27+$i # Uppercase (A = 65)
 }
