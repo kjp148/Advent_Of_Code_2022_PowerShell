d7a4bee7bae97c2ec0439f10b5fdde9c8fb03921
Keanan Paul <keananpaul@yahoo.com>
2022-12-03 02:35:12 -0800

    Finished day 2 p2.  Output is incorrect though.

--- day2.ps1
+++ day2.ps1
@@ -1,4 +1,5 @@
-$PointsTotal = 0
+$Part1PointsTotal = 0
+$Part2PointsTotal = 0
 Get-Content .\input\day2.txt | ForEach-Object {
     $OpponentMove = ($_ -split " ")[0]
     $MyMove = ($_ -split " ")[1]
@@ -8,28 +9,29 @@ Get-Content .\input\day2.txt | ForEach-Object {
         "A" {
             switch ($MyMove)
             {
-                "X" {$PointsTotal += 1 + 3}
-                "Y" {$PointsTotal += 2 + 6}
-                "Z" {$PointsTotal += 3}
+                "X" {$Part1PointsTotal += 1 + 3; $Part2PointsTotal += 2 + 0} # Rock/Lose(Paper)
+                "Y" {$Part1PointsTotal += 2 + 6; $Part2PointsTotal += 1 + 3} # Paper/Draw(Rock)
+                "Z" {$Part1PointsTotal += 3 + 0; $Part2PointsTotal += 3 + 6} # Scissors/Win(Scissors)
             }
         }
         "B" {
             switch ($MyMove)
             {
-                "X" {$PointsTotal += 1}
-                "Y" {$PointsTotal += 2 + 3}
-                "Z" {$PointsTotal += 3 + 6}
+                "X" {$Part1PointsTotal += 1 + 0; $Part2PointsTotal += 1 + 0} # Rock/Lose(Rock)
+                "Y" {$Part1PointsTotal += 2 + 3; $Part2PointsTotal += 2 + 3} # Paper/Draw(Paper)
+                "Z" {$Part1PointsTotal += 3 + 6; $Part2PointsTotal += 3 + 6} # Scissors/Win(Scissors)
             }
         }
         "C" {
             switch ($MyMove)
             {
-                "X" {$PointsTotal += 1 + 6}
-                "Y" {$PointsTotal += 2}
-                "Z" {$PointsTotal += 3 + 3}
+                "X" {$Part1PointsTotal += 1 + 6; $Part2PointsTotal += 2 + 0} # Rock/Lose(Paper)
+                "Y" {$Part1PointsTotal += 2 + 0; $Part2PointsTotal += 3 + 3} # Paper/Draw(Scissors)
+                "Z" {$Part1PointsTotal += 3 + 3; $Part2PointsTotal += 1 + 6} # Scissors/Win(Rock)
             }
         }
     }
 }
 
-Write-Host Day 2 p1: $PointsTotal
+Write-Host "Day 2 p1: $Part1PointsTotal"
+Write-Host "Day 2 p2: $Part2PointsTotal"
