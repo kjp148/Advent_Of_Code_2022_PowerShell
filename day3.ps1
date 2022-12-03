c42339fe8d6f5c03ae81f58d7eb142261bec5127
Keanan Paul <keananpaul@yahoo.com>
2022-12-03 04:15:07 -0800

    Day 3 p1 finished.

--- day3.ps1
+++ day3.ps1
@@ -1,15 +1,31 @@
 $RucksackArray = @() # Split into each rucksack with compartments a and b
 Get-Content .\Input\day3.txt | ForEach-Object {
     $RucksackArray += @{
-        a = $_.Substring(0, ($_.Length / 2));
-        b = $_.Substring($_.Length / 2)
+        a = $_.Substring(0, ($_.Length / 2)); # First half
+        b = $_.Substring($_.Length / 2) # Second half
     }
 }
 
 # Create hashtable of char values
-$CharTable = @{}
+$PriorityTable = @{}
 for ($i = 0; $i -lt 26; $i++)
 {
-    $CharTable.Add($([string][char](97+$i)), 1+$i) # Lowercase (a = 97)
-    $CharTable.$([string][char](65+$i)) = 27+$i # Uppercase (A = 65)
+    $PriorityTable.$("Lower" + [string][char](65+$i)) = 1+$i # Lowercase (a = 97)
+    $PriorityTable.$("Upper" + [string][char](65+$i)) = 27+$i # Uppercase (A = 65)
 }
+
+# Find letter that appears in both compartments
+$PrioritySum = 0
+foreach ($Rucksack in $RucksackArray)
+{
+    #Strip of duplicates then iterate through first compartment to see if any elements are in the second compartment
+    $Rucksack.a.ToCharArray() | Select-Object -Unique | ForEach-Object {
+        if ($Rucksack.b.Contains($_))
+        {
+            if ([int][char]$_ -gt 96) {$PrioritySum += $PriorityTable."Lower$($_)"}
+            else {$PrioritySum += $PriorityTable."Upper$($_)"}
+        }
+    }
+}
+
+Write-Host "Day 3 p1: $PrioritySum"
