5cfdd89d940c718a72a1d54cd8e9c6c72a25b90a
Keanan Paul <keananpaul@yahoo.com>
2022-12-04 09:38:24 -0800

    Added input into an array of a pair of pairs.

--- day4.ps1
+++ day4.ps1
@@ -5,3 +5,20 @@
         if Pair.First.Start - Pair.Second.Start -gt 0 AND Pair.First.End - Pair.Second.End -gt 0 OR (REVERSE pair.second.start - pair.first.start etc.)
             PairsEncapsulatingOther++
 #>
+
+# Seperate each line into an object with two pairs of pairs
+$PairsOfElves = @()
+foreach ($Line in (Get-Content .\Input\day4.txt))
+{
+    $SplitElves = ($Line -split ",")
+    $PairsOfElves += @{
+        First = @{
+            Start = [int]($SplitElves[0] -split "-")[0];
+            End = [int]($SplitElves[0] -split "-")[1]
+        };
+        Second = @{
+            Start = [int]($SplitElves[1] -split "-")[0];
+            End = [int]($SplitElves[1] -split "-")[1]
+        }
+    }
+}
