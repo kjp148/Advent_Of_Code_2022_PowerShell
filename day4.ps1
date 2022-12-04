72df37bbd5ce9a39f34600e3bbe7daa7c19405e2
Keanan Paul <keananpaul@yahoo.com>
2022-12-04 11:22:09 -0800

    Finished day 4 p1 using debug output and breakpoints.  The logic for FirstResult and SecondResult were incorrect.

--- day4.ps1
+++ day4.ps1
@@ -25,11 +25,14 @@ foreach ($Line in (Get-Content .\Input\day4.txt))
 
 # Count how many pairs overlap
 $OverlapCount = 0
+<#Debug#>$Overlap=$false
 foreach ($Pair in $PairsOfElves)
 {
-    $FirstResult = ((($Pair.First.Start - $Pair.Second.Start) -ge 0) -and (($Pair.First.End - $Pair.Second.End) -ge 0))
-    $SecondResult = ((($Pair.Second.Start - $Pair.First.Start) -ge 0) -and (($Pair.Second.End - $Pair.First.End) -ge 0))
-    if ($FirstResult -or $SecondResult) {$OverlapCount++}
+    $FirstResult = (($Pair.First.Start -le $Pair.Second.Start) -and ($Pair.First.End -ge $Pair.Second.End))
+    $SecondResult = (($Pair.Second.Start -le $Pair.First.Start) -and ($Pair.Second.End -ge $Pair.First.End))
+    if ($FirstResult -or $SecondResult) {$OverlapCount++; <#Debug#>$Overlap=$true}
+    Write-Host "Debug: $($Pair.First.Start)-$($Pair.First.End),$($Pair.Second.Start)-$($Pair.Second.End) = $Overlap | $OverlapCount"
+    <#Debug#>$Overlap=$false
 }
 
 Write-Host "Day 4 p1: $OverlapCount"
