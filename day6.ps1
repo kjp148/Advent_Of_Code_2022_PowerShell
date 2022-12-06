952a87428d425951679dd25c703b1a7b189ef603
Keanan Paul <keananpaul@yahoo.com>
2022-12-06 10:53:57 -0800

    Finished day 6 p2.

--- day6.ps1
+++ day6.ps1
@@ -1,10 +1,21 @@
 $CharacterArray = (Get-Content .\Input\day6.txt -Raw).Trim().ToCharArray()
-
+$Part1Done = $false
+$Part2Done = $false
 for ($i = 3; $i -lt $CharacterArray.Length; $i++)
 {
-    if (($CharacterArray[($i - 3)..$i] | Select-Object -Unique).Length -eq 4)
+    # Part 1
+    if (!$Part1Done -and ($CharacterArray[($i - 3)..$i] | Select-Object -Unique).Length -eq 4)
     {
         Write-Host "Day 6 p1: $($i + 1)"
-        break
+        $Part1Done = $true
     }
+
+    # Part 2
+    if (!$Part2Done -and ($CharacterArray[($i - 13)..$i] | Select-Object -Unique).Length -eq 14)
+    {
+        Write-Host "Day 6 p2: $($i + 1)"
+        $Part2Done = $true
+    }
+
+    if ($Part1Done -and $Part2Done) {break}
 }
