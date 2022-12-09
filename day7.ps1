edb34bcfbbba57a248f43f9dd0a9a9c2eff6b16e
Keanan Paul <keananpaul@yahoo.com>
2022-12-09 14:36:27 -0800

    Finished navigation of folders.

--- day7.ps1
+++ day7.ps1
@@ -4,14 +4,24 @@ foreach ($Command in (Get-Content .\Input\day7.txt))
     switch (($Command -split " ")[0])
     {
         "$" {
-            switch (($Command -split " ")[1])
+            if (($Command -split " ")[1] -eq "cd")
             {
-                "cd" {Write-Host $Command}
-                "ls" {Write-Host $Command}
+                switch (($Command -split " ")[2])
+                {
+                    ".." {
+                        # Remove last folder from current path
+                        $SplitFolders = $CurrentFolder -split "/"
+                        $CurrentFolder = ($SplitFolders[0..($SplitFolders.Length - 3)] -join "/") + "/"
+                    }
+                    "/" { # Handling for root
+                        $CurrentFolder = "/"
+                    }
+                    default {$CurrentFolder += "$(($Command -split " ")[2])/"} # Add folder to current path
+                }
+                # Looks like we can ignore the ls command "ls" {#List}
             }
         }
-        "dir" {
-            
-        }
+        "dir" {}#Folder
+        default {}#File
     }
 }
