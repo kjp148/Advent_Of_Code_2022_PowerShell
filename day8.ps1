92a66025b452dffb0fb7c061a06348fe54844737
Keanan Paul <keananpaul@yahoo.com>
2022-12-11 22:10:30 -0800

    Counted edge trees.

--- day8.ps1
+++ day8.ps1
@@ -9,3 +9,16 @@ for ($y = 0; $y -lt $yMax; $y++) # Populate 2d array with coords
         $TreeGrid[$x, $y] = $RawInput[$y].ToCharArray()[$x]
     }
 }
+
+# Count trees visible from the edge
+$VisibleCount = 0
+for ($y = 0; $y -lt $yMax; $y++)
+{
+    for ($x = 0; $x -lt $xMax; $x++)
+    {
+        if (($x -eq 0) -or ($x -eq $xMax - 1) -or ($y -eq 0) -or ($y -eq $yMax - 1)) # Check for edge tree
+        {
+            $VisibleCount++
+        }
+    }
+}
