3bdf23609e6b5a31f412c80ea9aa1ce6471e88a1
Keanan Paul <keananpaul@yahoo.com>
2022-12-12 04:37:45 -0800

    Finished checking view scores for every tree, but the output is incorrect.

--- day8.ps1
+++ day8.ps1
@@ -1,7 +1,7 @@
 $RawInput = Get-Content .\Input\day8.txt
 $xMax = $RawInput[0].Length # Left to right in the file
 $yMax = $RawInput.Length # Top to bottom in the file
-$TreeGrid = New-Object 'object[,]' $xMax, $yMax # Create 2d array
+$TreeGrid = New-Object 'object[,]' $xMax, $yMax # Create 2d array for grid of trees
 for ($y = 0; $y -lt $yMax; $y++) # Populate 2d array with coords
  {
     for ($x = 0; $x -lt $xMax; $x++)
@@ -10,7 +10,7 @@ for ($y = 0; $y -lt $yMax; $y++) # Populate 2d array with coords
     }
 }
 
-# Count trees visible from the edge
+# Part 1 - Count trees visible from the edge
 $VisibleCount = 0
 for ($y = 0; $y -lt $yMax; $y++)
 {
@@ -58,3 +58,48 @@ for ($y = 0; $y -lt $yMax; $y++)
 }
 
 Write-Host "Day 8 p1: $VisibleCount"
+
+# Part 2
+$ViewGrid = New-Object 'object[,]' $xMax, $yMax # Create 2d array for view score from each tree
+for ($y = 0; $y -lt $yMax; $y++)
+{
+    for ($x = 0; $x -lt $xMax; $x++)
+    {
+        $Up = $Down = $Left = $Right = 0
+        if ($x -ne 0) # Check left
+        {
+            for ($xSearchIterate = $x - 1; $xSearchIterate -ge 0; $xSearchIterate--)
+            {
+                $Left++
+                if ($TreeGrid[$xSearchIterate, $y] -ge $TreeGrid[$x, $y]) {break}
+            }
+        }
+        if ($x -ne $xMax - 1) # Check right
+        {
+            for ($xSearchIterate = $x + 1; $xSearchIterate -le $xMax; $xSearchIterate++)
+            {
+                $Right++
+                if ($TreeGrid[$xSearchIterate, $y] -ge $TreeGrid[$x, $y]) {break}
+            }
+        }
+        if ($y -ne 0) # Check up
+        {
+            for ($ySearchIterate = $y - 1; $ySearchIterate -ge 0; $ySearchIterate--)
+            {
+                $Up++
+                if ($TreeGrid[$x, $ySearchIterate] -ge $TreeGrid[$x, $y]) {break}
+            }
+        }
+        if ($y -ne $yMax - 1) # Check down
+        {
+            for ($ySearchIterate = $y + 1; $ySearchIterate -le $yMax; $ySearchIterate++)
+            {
+                $Down++
+                if ($TreeGrid[$x, $ySearchIterate] -ge $TreeGrid[$x, $y]) {break}
+            }
+        }
+        $ViewGrid[$x, $y] = $Up * $Down * $Left * $Right
+    }
+}
+
+Write-Host "Day 8 p2: $(($ViewGrid | Sort-Object -Descending)[0])"
